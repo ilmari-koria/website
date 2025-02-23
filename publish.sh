14f52b7947b2e55f788f474e4faa0e9824beed84
ilmari-koria <ilmarikoria@posteo.net>
2025-02-23 15:39:09 +0000

    clean dir

--- publish.sh
+++ publish.sh
@@ -4,16 +4,11 @@ ORG_FILES="/home/ilmari/my-files/website/org"
 PUBLISH_DIR="./publish"
 BIBTEX="./bibtex"
 XQ="./xq"
-# convert org files
-# $BASEX -bpath-to-org-files=$ORG_FILES $XQ/convert-org-files.xq
-# # update org files in basex
-# $BASEX -bpath-to-org-files=$ORG_FILES $XQ/update-org-files.xq
-# transform org files and move to $publish
-
-# bibtex2html --style "ieeetr" --footer "" --header "" --no-header --nodoc --nobibsource --ignore-errors --sort-as-bibtex --output $BIBTEX/bibliography $BIBTEX/bibliography.bib
-# tidy -q --numeric-entities yes -asxhtml -o $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
-# $BASEX -bpublish-path=$PUBLISH_DIR $XQ/transform-org-files-and-publish.xq
-# rm $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
-
-# resume
+$BASEX -bpath-to-org-files=$ORG_FILES $XQ/convert-org-files.xq
+$BASEX -bpath-to-org-files=$ORG_FILES $XQ/update-org-files.xq
+bibtex2html --style "ieeetr" --footer "" --header "" --no-header --nodoc --nobibsource --ignore-errors --sort-as-bibtex --output $BIBTEX/bibliography $BIBTEX/bibliography.bib
+tidy -q --numeric-entities yes -asxhtml -o $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
+$BASEX -bpublish-path=$PUBLISH_DIR $XQ/transform-org-files-and-publish.xq
+rm $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
+$BASEX -bpublish-path=$PUBLISH_DIR $XQ/publish-static-pages.xq
 $BASEX -bpublish-path=$PUBLISH_DIR $XQ/generate-resume.xq
