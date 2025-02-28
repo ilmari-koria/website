2cf29725956bee5d90a299fd15467abd149266d2
ilmari-koria <ilmarikoria@posteo.net>
2025-02-28 21:01:15 +0000

    adjust script

--- publish.sh
+++ publish.sh
@@ -8,11 +8,15 @@ BIBTEX="./bibtex"
 
 bibtex2html --style "ieeetr" --footer "" --header "" --no-header --nodoc --nobibsource --ignore-errors --sort-as-bibtex --output $BIBTEX/bibliography $BIBTEX/bibliography.bib
 tidy -q --numeric-entities yes -asxhtml -o $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
+
 $BASEX -bpath-to-org-files=$ORG_FILES \
        -bpublish-path=$PUBLISH_DIR \
        -c $BASEX_DIR/repo/blog/content/publish.bxs
-rm $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
-rm $PUBLISH_DIR/*.log $PUBLISH_DIR/*.out $PUBLISH_DIR/*.aux
+
 rsync -avz --delete $PUBLISH_DIR/ root@ilmarikoria.xyz:/var/www/ilmarikoria/
 sitemap-generator -l https://ilmarikoria.xyz
 rsync -avz --delete sitemap.xml root@ilmarikoria.xyz:/var/www/ilmarikoria/sitemap.xml
+
+rm $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
+rm $PUBLISH_DIR/*.log $PUBLISH_DIR/*.out $PUBLISH_DIR/*.aux
+rm ./sitemap.xml
