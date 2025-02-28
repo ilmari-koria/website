6aefc0c7424f56f8c0d5933192a13c45f162e35d
ilmari-koria <ilmarikoria@posteo.net>
2025-02-28 20:29:17 +0000

    adjust paths

--- publish.sh
+++ publish.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# TODO set basex home dir properly
-# TODO convert part of this into a BaseX .bxs command script:
-# --> https://docs.basex.org/main/Commands#command_scripts
 BASEX="./basex/bin/basex"
-# TODO this should be a relative path?
-ORG_FILES="/home/ilmari/my-files/website/org"
+BASEX_DIR="./basex"
+ORG_FILES="./org"
 PUBLISH_DIR="./publish"
 BIBTEX="./bibtex"
-XQ="./xq"
-$BASEX -bpath-to-org-files=$ORG_FILES $XQ/convert-org-files.xq
-$BASEX -bpath-to-org-files=$ORG_FILES $XQ/update-org-files.xq
+
 bibtex2html --style "ieeetr" --footer "" --header "" --no-header --nodoc --nobibsource --ignore-errors --sort-as-bibtex --output $BIBTEX/bibliography $BIBTEX/bibliography.bib
 tidy -q --numeric-entities yes -asxhtml -o $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
-$BASEX -bpublish-path=$PUBLISH_DIR $XQ/transform-org-files-and-publish.xq
+$BASEX -bpath-to-org-files=$ORG_FILES \
+       -bpublish-path=$PUBLISH_DIR \
+       -c $BASEX_DIR/repo/blog/content/publish.bxs
 rm $BIBTEX/bibliography.xml $BIBTEX/bibliography.html
-$BASEX -bpublish-path=$PUBLISH_DIR $XQ/publish-static-pages.xq
-$BASEX -bpublish-path=$PUBLISH_DIR $XQ/generate-resume.xq
 rm $PUBLISH_DIR/*.log $PUBLISH_DIR/*.out $PUBLISH_DIR/*.aux
-rsync -avz --delete $PUBLISH_DIR/ root@ilmarikoria.xyz:/var/www/ilmarikoria/
-sitemap-generator -l https://ilmarikoria.xyz
-rsync -avz --delete sitemap.xml root@ilmarikoria.xyz:/var/www/ilmarikoria/sitemap.xml
+# rsync -avz --delete $PUBLISH_DIR/ root@ilmarikoria.xyz:/var/www/ilmarikoria/
+# sitemap-generator -l https://ilmarikoria.xyz
+# rsync -avz --delete sitemap.xml root@ilmarikoria.xyz:/var/www/ilmarikoria/sitemap.xml
